f0a1a9bfc4a72aff60ec5111b8d502a8da3d0439
Giovanni Collazo <hello@gcollazo.com>
2017-05-26 19:41:07 -0400

    FSN modifications

--- FullScreenDetector/MDCAppDelegate.m
+++ FullScreenDetector/MDCAppDelegate.m
@@ -14,6 +14,8 @@
 
 @implementation MDCAppDelegate
 
+NSDate *lastAlert;
+
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(switchedToFullScreenApp:) name:kMDCFullScreenDetectorSwitchedToFullScreenApp object:nil];
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(switchedToRegularSpace:) name:kMDCFullScreenDetectorSwitchedToRegularSpace object:nil];
@@ -22,7 +24,7 @@
 - (void)showNotification:(NSString *)message {
   NSLog(@"%@", message);
   NSUserNotification *notification = [[NSUserNotification alloc] init];
-  notification.title = @"Full Screen Status Changed";
+  notification.title = @"WARNING!!!";
   notification.informativeText = message;
   notification.soundName = nil;
 
@@ -30,11 +32,52 @@
 }
 
 - (void)switchedToFullScreenApp:(NSNotification *)n {
-  [self showNotification:@"On a full screen app"];
+    NSTimeInterval timeInterval = [lastAlert timeIntervalSinceNow];
+    
+    NSLog(@"%f", timeInterval);
+    
+    if (timeInterval == 0.0 || timeInterval < -60) {
+        NSLog(@"Show alert!");
+        lastAlert = [NSDate date];
+        
+        NSString *message = @"REMEMBER TO START SCREEN RECORDING";
+        NSAlert *alert = [NSAlert alertWithMessageText: message
+                                         defaultButton: @"Ok"
+                                       alternateButton: nil
+                                           otherButton: nil
+                             informativeTextWithFormat: @"Make sure you future self is happy. Open QuickTime and start recorging the screen."];
+        
+        
+        
+        // Show notification center message
+        [self showNotification: message];
+        
+        // Make sound
+        NSSound *sound = [[NSSound alloc] initWithContentsOfFile:[[NSBundle mainBundle]
+                                                 pathForResource:@"alarm"
+                                                          ofType:@"mp3"] byReference:NO];
+        [sound play];
+        
+        // Say messsage
+        NSSpeechSynthesizer *synth = [[NSSpeechSynthesizer alloc] initWithVoice:@"com.apple.speech.synthesis.voice.Alex"];
+        [synth startSpeakingString:message];
+        
+        // Bounce dock icon
+        [NSApp requestUserAttention:NSCriticalRequest];
+        
+        // Show modal alert
+        [alert runModal];
+        
+        // Active app
+        [[NSRunningApplication currentApplication] activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+        [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
+    } else {
+        NSLog(@"Dont't show alert!");
+    }
 }
 
 - (void)switchedToRegularSpace:(NSNotification *)n {
-  [self showNotification:@"On a regular space"];
+  // [self showNotification:@"On a regular space"];
 }
 
 @end
